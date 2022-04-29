df342a1b6311cd7e34b2eec08234545d13766436
Andreas <af222ud@student.lnu.se>
2022-04-29 17:00:45 +0200

    Implement table creation functionality

--- databaseManager.py
+++ databaseManager.py
@@ -8,7 +8,7 @@ def connectToDatabase():
     global cursor
 
     # Attempts to connect to the specified database.
-    cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='karlsson_franzen_pa2')
+    cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='karlsson_franzén_pa2')
     cursor = cnx.cursor()
 
     return cnx
@@ -24,10 +24,43 @@ def connectToDatabase():
       print("Database was created successfully.")
 
       # Connect to the newly created database.
-      cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='karlsson_franzén')
+      cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='karlsson_franzén_pa2')
       cursor = cnx.cursor()
 
       return cnx
     except:
       print('There was an error when attempting to create the database or connect to mySQL. The application will now close.')
       sys.exit()
+
+def createTables():
+  global cursor
+
+  # Creates the "phones" table, if it does not already exist.
+  phones = """CREATE TABLE IF NOT EXISTS phones (
+                name varchar(255) COLLATE utf8mb4_unicode_ci,
+                year int,
+                soc varchar(255),
+                maker varchar(255),
+                battery varchar(255),
+                PRIMARY KEY (name));"""
+
+  # Creates the "reviews" table, if it does not already exist.
+  reviews = """CREATE TABLE IF NOT EXISTS reviews (
+                model varchar(255) COLLATE utf8mb4_unicode_ci,
+                reviewer varchar(255),
+                organization varchar(255),
+                score int,
+                PRIMARY KEY(model, organization));"""
+
+  # Creates the "SoCs" table, if it does not already exist.
+  socs = """CREATE TABLE IF NOT EXISTS socs (
+              name varchar(255),
+              clock_speed varchar(255),
+              cores int,
+              gpu varchar(255),
+              PRIMARY KEY(name));"""
+
+  # Execute the queries.
+  cursor.execute(phones)
+  cursor.execute(reviews)
+  cursor.execute(socs)
--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import databaseManager
 
 # Attempt to connect to the database.
 connection = databaseManager.connectToDatabase()
+
+# Attempts to create the tables needed.
+databaseManager.createTables()
+
+connection.commit()
+connection.close()
